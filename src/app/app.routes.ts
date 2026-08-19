0666f0842f8c8e858640cc8d8807d6ec5aa466ac
Haseeb-Persistent <haseebpersistent@gmail.com>
2026-08-19 17:02:58 +0500

    asdsadasd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,7 @@ import { BlogComponent } from './components/blog/blog.component';
 import { About } from './components/about/about';
 import { PaymentComponent } from './components/PaymentComponent/PaymentComponent.component';
 import { IvfVsAiComponent } from './components/ivf-vs-ai-vf/ivf-vs-ai-vf.component';
-import { AiIvfComponent } from './components/SERVICE_MODULE/ai-ivf/ai-ivf.component';
 import { Services } from './components/services/services';
-import { ServiceRoute } from './components/SERVICE_MODULE/service.route';
 import { CarsSectionComponent } from './components/cars-section/cars-section.component';
 
 export const routes: Routes = [
@@ -23,7 +21,6 @@ export const routes: Routes = [
      { path: 'payment/:appointmentNumber', component: PaymentComponent }, 
      { path: 'IVF-VS-AI-IVF', component: IvfVsAiComponent }, 
      
-     ...ServiceRoute,
 
      { path: '**', redirectTo: 'RENT-A-CAR' }
 ];
